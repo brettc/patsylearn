82f605a2bbcc98ffb8ae9a1a1c38b5a281b9f22d
Brett Calcott <brett.calcott@gmail.com>
2020-11-27 10:23:12 +1300

    Add requirements to setup. Make license agree.

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ import patsylearn
 from os.path import exists
 from setuptools import setup
 
+install_requires = open("requirements.txt").read().strip().split("\n")
+
 setup(name='patsylearn',
       version=patsylearn.__version__,
       description='Scikit-lean Patsy adaptor',
       url='http://github.com/amueller/patsylearn/',
       maintainer='Andreas Mueller',
       maintainer_email='t3kcit@gmail.com',
-      license='BSD',
+      license='GPL',
+      install_requires=install_requires,
       keywords='scikit-learn patsy formula machine-learning',
       packages=['patsylearn'],
       long_description=(open('README.md').read() if exists('README.rst') else
